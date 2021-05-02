ebe6feb30ffdc5c6575f0c0665e405264b2687a9
Juan Jose Mayorga <tmaniacus@gmail.com>
2021-05-02 12:13:34 -0500

    server created

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT;
+const Server = require('./models/server');
+const server = new Server();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.listen(port, () => {
-  console.log(`App listening at port ${port}`);
-});
+server.listen();
